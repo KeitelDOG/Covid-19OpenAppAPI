1c92f8de8044fb27df0b25dd01853cefbc7762a6
Keitel Jovin <keiteldog@gmail.com>
2020-05-27 13:18:10 -0400

    Add static Questions seeds and remove other seeds

--- database/seeds/00200_questions.js
+++ database/seeds/00200_questions.js
@@ -2,16 +2,50 @@ var faker = require('faker/locale/en');
 
 exports.seed = function(knex, Promise) {
   // Inserts seed entries
-  let models = [];
-
-  for (let i = 0; i < 40; i++) {
-    models.push({
-      title: faker.lorem.sentence().slice(0, 200),
-      type: parseInt(Math.random() * 4 + 1),
-      choice: faker.lorem.words().slice(0, 400),
-
-    });
-  }
+  let models = [
+    {
+      id: 1,
+      title: "Quels sont vos symptômes?",
+      type: 1,
+      choice: "Fièvre,Toux,Fatigue,Difficulté Respiratoire,Aucun,Autres",
+    },
+    {
+      id: 2,
+      title: "Depuis quand ressentez vous ces symptômes?",
+      type: 2,
+      choice: "Jours,Semaines,Mois,Année",
+    },
+    {
+      id: 3,
+      title: "Avez-vous une de ces pathologies ( Antécédent médical ) ci-dessous?",
+      type: 1,
+      choice: "HTA,Diabète,Asthme,Cancer,Aucun,Autres",
+    },
+    {
+      id: 4,
+      title: "Est-ce que vous avez un parent qui ont ou moins une de ces pathologies ci-dessous?",
+      type: 1,
+      choice: "HTA,Diabète,Asthme,Cancer,Aucun,Autres",
+    },
+    {
+      id: 4,
+      title: "Habitudes ( Avez-vous l’habitude de consommer ces substances)?",
+      type: 1,
+      choice: "Alcool,Tabac,Mariguana,Café,Aucun,Autres",
+    },
+    {
+      id: 5,
+      title: "Quel est votre groupe sanguin?",
+      type: 2,
+      choice: "A+, A-, B+, B-, AB+, AB-, O+, O-",
+    },
+    {
+      id: 6,
+      title: "Avez-vous une allergie ? si oui précisez ?",
+      type: 3,
+      choice: "",
+    }
+  ];
 
   return knex('questions').insert(models);
 };
